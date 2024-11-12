af3da1d375f7dbf07960123e5d67232cc492a888
Parth Tate <113414261+Parthtate@users.noreply.github.com>
2024-11-12 09:50:15 +0530

    Update DPKnapsackProbole.py

--- DPKnapsackProbole.py
+++ DPKnapsackProbole.py
@@ -1,28 +1,19 @@
-def knapsck(values, weights, max_weight):
+def knapsack(values, weights, max_weight):
     n = len(values)
-    dp = [[0] * (max_weight + 1) for _ in range(n+1)]
+    dp = [[0] * (max_weight + 1) for _ in range(n + 1)]
     
     for i in range(1, n + 1):
         for w in range(1, max_weight + 1):
-            
             if weights[i - 1] <= w:
-                dp[i][w] = max(dp[i - 1] [w], dp[i - 1] [w - weights[i - 1]] + values[i-1])
-                
+                dp[i][w] = max(dp[i - 1][w], dp[i - 1][w - weights[i - 1]] + values[i - 1])
             else:
                 dp[i][w] = dp[i - 1][w]
                 
     return dp[n][max_weight]
     
+# Example usage
 values = [28, 24, 15]
 weights = [18, 15, 10]
 max_weight = 20
 
-print("max profit is: ", knapsck(values, weights, max_weight))
-
-
-
-
-
-    
-                
-                
+print("Max profit is:", knapsack(values, weights, max_weight))
